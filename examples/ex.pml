976fd56f4a3ade8d273c73bc58b92605d9176666
sndtkrh <sndtkrh@gmail.com>
2018-04-24 11:25:25 +0900

    Implement file mode

--- examples/ex.pml
+++ examples/ex.pml
@@ -1,7 +1,19 @@
+Axiom p->(q->(p/\q))
+G #0
 Axiom [](p->q)->([]p->[]q)
-Axiom (p->q->r)->(p->q)->(p->r)
-Axiom T
-Axiom T->T
-US #1 (p->q) q
-MP #3 #4
-G #1
+US #2 q->(p/\q) q
+MP #1 #3
+US #2 r q
+US #5 q p
+US #6 p/\q r
+Axiom (a->b)->(b->c)->(a->c)
+US #8 []p a
+US #9 [](q->(p/\q)) b
+US #10 []q->[](p/\q) c
+MP #4 #11
+MP #7 #12
+Axiom (a->(b->c))->((a/\b)->c)
+US #14 []p a
+US #15 []q b
+US #16 [](p/\q) c
+MP #13 #17
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
@@ -6,16 +7,26 @@
 using namespace pml;
 
 void interactive_mode();
-int main() {
+void filechecking(const std::string & filename);
+int main(int argc, char *argv[]) {
+  if( argc == 1 ) {
+    interactive_mode();
+  } else if( argc == 2 ) {
+    filechecking(argv[1]);
+  } else {
+    std::cout << "Usage :" << std::endl;
+    std::cout << "    pml              [interactive mode]" << std::endl;
+    std::cout << "    pml <filename>   [cheking proof of the <filename>]" << std::endl;
+  }
+}
+
+void interactive_mode() {
   std::cout << "PML -- A Proof Assistant for Modal Logic" << std::endl;
   std::cout << "Axioms are :" << std::endl;
   for(const auto & axiom : AxiomK) {
     std::cout << axiom->to_string() << std::endl;
   }
-  interactive_mode();
-}
 
-void interactive_mode() {
   std::vector<Formula *> theorems;
   std::string command;
   std::cout << ">> " << std::flush;
@@ -30,4 +41,34 @@ void interactive_mode() {
     std::cout << ">> " << std::flush;
   }
   std::cout << std::endl;
+}
+
+void filechecking(const std::string & filename) {
+  std::vector<Formula *> theorems;
+  std::ifstream fin;
+  fin.open(filename, std::ios::in);
+  if( !fin ){
+    std::cout << "Error : cannot open the file " << filename << "." << std::endl;
+    exit(1);
+  }
+  bool failed = false;
+  std::string source;
+  int line = 1;
+  while(std::getline(fin, source)) {
+    std::size_t p = 0;
+    if( command_parser(source, p, theorems) ){
+      std::size_t i = theorems.size() - 1;
+      std::cout << "#" << i << "  |- " << theorems[i]->to_string() << std::endl;
+    } else {
+      std::cout << "Failed at line " << line << std::endl;
+      failed = true;
+      break;
+    }
+    line++;
+  }
+  if( failed ) {
+    std::cout << "\x1b[32mERROR\x1b[39m" << std::endl;
+  } else {
+    std::cout << "\x1b[32mOK\x1b[39m" << std::endl;
+  }
 }
